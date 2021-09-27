ff785e0cf8ef97b585c81fc44c26c0efd9e20230
Aleksandr Maksimov <amaksimov744@gmail.com>
2021-09-27 21:10:51 +0300

    Что то переписал

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-message = 'Hello world'
+ask_number = 45
+user_answer = int(input('Введи загаданное число: '))
 
-print(message)
-
-
+if user_answer == ask_number:
+    print('Маладец! Угадал')
+elif user_answer < ask_number:
+    print('Малавата будет')
+else:
+    print('эка ты маханул')
