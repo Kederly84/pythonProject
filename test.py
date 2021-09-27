1a1a242d4610e0a39b7626ef3d1ae6d739498e01
Kederly84 <90934203+Kederly84@users.noreply.github.com>
2021-09-27 22:53:30 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-ask_number = 45
-user_answer = int(input('Введи загаданное число: '))
-
-if user_answer == ask_number:
-    print('Маладец! Угадал')
-elif user_answer < ask_number:
-    print('Малавата будет')
-else:
-    print('эка ты маханул')
