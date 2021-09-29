9c4ff03978ac5d7e9ad5f9998076288afb4a5b81
Aleksandr Maksimov <amaksimov744@gmail.com>
2021-09-29 21:53:13 +0300

    Цикл for in

---
+++ Lesson 4.py
@@ -0,0 +1,23 @@
+friend_name = 'Max'
+friends = ['Max', 'Leo', 'Kate']
+roles = ('admin', 'guest', 'user')
+
+# Цикл for in
+
+for friend in friends:
+    print(friend)
+
+for letter in friend_name:
+    print(letter)
+
+for role in roles:
+    print(role)
+
+# Цикл while
+#
+# i = 0
+#
+# while i < len(friends):
+#     friend = friends[i]
+#     print(friend)
+#     i += 1
